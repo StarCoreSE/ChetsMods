1631f1f772192e07e2108887c4fdfe33f9c22b5c
CommodoreChet <50426713+CommodoreChet@users.noreply.github.com>
2023-10-17 13:32:54 -0400

    Minor Update
    [h1]Changed:[/h1]
    Simplified some Logic
    Adjusted Code to read from init values rather than magic numbers
    
    [h1]New:[/h1]
    Add a Config File
     - This File can be located in your Saves Storage Folder. Power Draw, Range, and Stock Limits can all be adjusted

---
+++ Quantum Tether/Data/Scripts/TetherBlock/Tether_Config.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using Sandbox.ModAPI;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame.Utilities; // this ingame namespace is safe to use in mods as it has nothing to collide with
+using VRage.Utils;
+
+namespace InventoryTether.Config
+{
+    // This example is minimal code required for it to work and with comments so you can better understand what is going on.
+
+    // The gist of it is: ini file is loaded/created that admin can edit, SetVariable is used to store that data in sandbox.sbc which gets automatically sent to joining clients.
+    // Benefit of this is clients will be getting this data before they join, very good if you need it during LoadData()
+    // This example does not support reloading config while server runs, you can however implement that by sending a packet to all online players with the ini data for them to parse.
+
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class Tether_BasicConfig : MySessionComponentBase
+    {
+        Tether_ConfigSettings Config = new Tether_ConfigSettings();
+
+        public override void LoadData()
+        {
+            Config.Load();
+
+            // example usage/debug
+            MyLog.Default.WriteLineAndConsole($"### SomeNumber value={Config.MinimumPowerRequirement}");
+            MyLog.Default.WriteLineAndConsole($"### ToggleThings value={Config.MaximumPowerRequirement}");
+            MyLog.Default.WriteLineAndConsole($"### ToggleThings value={Config.MinBlockRange}");
+            MyLog.Default.WriteLineAndConsole($"### ToggleThings value={Config.MaxBlockRange}");
+            MyLog.Default.WriteLineAndConsole($"### ToggleThings value={Config.MinStockAmount}");
+            MyLog.Default.WriteLineAndConsole($"### ToggleThings value={Config.MaxStockAmount}");
+        }
+    }
+
+    public class Tether_ConfigSettings
+    {
+        const string VariableId = nameof(Tether_BasicConfig); // IMPORTANT: must be unique as it gets written in a shared space (sandbox.sbc)
+        const string FileName = "Quantum_Tether_Config.ini"; // the file that gets saved to world storage under your mod's folder
+        const string IniSection = "Config";
+
+        // settings you'd be reading, and their defaults.
+        public float MinimumPowerRequirement = 1.0f;
+        public float MaximumPowerRequirement = 50f;
+
+        public float MinBlockRange = 5;
+        public float MaxBlockRange = 500;
+
+        public float MinStockAmount = 1;
+        public float MaxStockAmount = 50;
+
+        void LoadConfig(MyIni iniParser)
+        {
+            // repeat for each setting field
+            MinimumPowerRequirement = iniParser.Get(IniSection, nameof(MinimumPowerRequirement)).ToSingle(MinimumPowerRequirement);
+            MaximumPowerRequirement = iniParser.Get(IniSection, nameof(MaximumPowerRequirement)).ToSingle(MaximumPowerRequirement);
+            MinBlockRange = iniParser.Get(IniSection, nameof(MinBlockRange)).ToSingle(MinBlockRange);
+            MaxBlockRange = iniParser.Get(IniSection, nameof(MaxBlockRange)).ToSingle(MaxBlockRange);
+            MinStockAmount = iniParser.Get(IniSection, nameof(MinStockAmount)).ToSingle(MinStockAmount);
+            MaxStockAmount = iniParser.Get(IniSection, nameof(MaxStockAmount)).ToSingle(MaxStockAmount);
+        }
+
+        void SaveConfig(MyIni iniParser)
+        {
+            // repeat for each setting field
+            iniParser.Set(IniSection, nameof(MinimumPowerRequirement), MinimumPowerRequirement);
+            iniParser.SetComment(IniSection, nameof(MinimumPowerRequirement), "Default: 1.0f - Minimum Required Power at the Lowest Range in MW"); // optional
+
+            iniParser.Set(IniSection, nameof(MaximumPowerRequirement), MaximumPowerRequirement);
+            iniParser.SetComment(IniSection, nameof(MaximumPowerRequirement), "Default: 50.0f - Maximum Required Power at the Longest Range in MW"); // optional
+
+            iniParser.Set(IniSection, nameof(MinBlockRange), MinBlockRange);
+            iniParser.SetComment(IniSection, nameof(MinBlockRange), "Default: 5 - Minimum Block Range in Meters as a Diameter"); // optional
+
+            iniParser.Set(IniSection, nameof(MaxBlockRange), MaxBlockRange);
+            iniParser.SetComment(IniSection, nameof(MaxBlockRange), "Default: 500 - Maximum Block Range in Meters as a Diameter"); // optional
+
+            iniParser.Set(IniSection, nameof(MinStockAmount), MinStockAmount);
+            iniParser.SetComment(IniSection, nameof(MinStockAmount), "Default: 1 - Minimum Stock Amount for Components"); // optional
+
+            iniParser.Set(IniSection, nameof(MaxStockAmount), MaxStockAmount);
+            iniParser.SetComment(IniSection, nameof(MaxStockAmount), "Default: 50 - Maximum Stock Amount for Components"); // optional
+
+        }
+
+        // nothing to edit below this point
+
+        public Tether_ConfigSettings()
+        {
+        }
+
+        public void Load()
+        {
+            if(MyAPIGateway.Session.IsServer)
+                LoadOnHost();
+            else
+                LoadOnClient();
+        }
+
+        void LoadOnHost()
+        {
+            MyIni iniParser = new MyIni();
+
+            // load file if exists then save it regardless so that it can be sanitized and updated
+
+            if(MyAPIGateway.Utilities.FileExistsInWorldStorage(FileName, typeof(Tether_ConfigSettings)))
+            {
+                using(TextReader file = MyAPIGateway.Utilities.ReadFileInWorldStorage(FileName, typeof(Tether_ConfigSettings)))
+                {
+                    string text = file.ReadToEnd();
+
+                    MyIniParseResult result;
+                    if(!iniParser.TryParse(text, out result))
+                        throw new Exception($"Config error: {result.ToString()}");
+
+                    LoadConfig(iniParser);
+                }
+            }
+
+            iniParser.Clear(); // remove any existing settings that might no longer exist
+
+            SaveConfig(iniParser);
+
+            string saveText = iniParser.ToString();
+
+            MyAPIGateway.Utilities.SetVariable<string>(VariableId, saveText);
+
+            using(TextWriter file = MyAPIGateway.Utilities.WriteFileInWorldStorage(FileName, typeof(Tether_ConfigSettings)))
+            {
+                file.Write(saveText);
+            }
+        }
+
+        void LoadOnClient()
+        {
+            string text;
+            if(!MyAPIGateway.Utilities.GetVariable<string>(VariableId, out text))
+                throw new Exception("No config found in sandbox.sbc!");
+
+            MyIni iniParser = new MyIni();
+            MyIniParseResult result;
+            if(!iniParser.TryParse(text, out result))
+                throw new Exception($"Config error: {result.ToString()}");
+
+            LoadConfig(iniParser);
+        }
+    }
+}
--- Quantum Tether/Data/Scripts/TetherBlock/Tether_GameLogic.cs
+++ Quantum Tether/Data/Scripts/TetherBlock/Tether_GameLogic.cs
@@ -24,6 +24,7 @@ using VRage.ObjectBuilders;
 using VRage.Utils;
 using InventoryTether.Particle;
 using static VRageRender.MyBillboard;
+using InventoryTether.Config;
 
 namespace InventoryTether
 {
@@ -36,16 +37,20 @@ namespace InventoryTether
 
         public MyPoweredCargoContainerDefinition InventoryTetherBlockDef;
         public TetherBlockSettings Settings = new TetherBlockSettings();
+        public readonly Tether_ConfigSettings Config = new Tether_ConfigSettings();
         public InventoryTetherMod Mod => InventoryTetherMod.Instance;
         private MyResourceSinkComponent Sink = null;
         public readonly Guid Settings_GUID = new Guid("47114A7D-B546-4C05-9E6E-2DFE3449E176");
 
-        private float MinBlockRange = 5;
-        private float MaxBlockRange = 500;
+        private float MinimumPowerRequirement;
+        private float MaximumPowerRequirement;
+
+        private float MinBlockRange;
+        private float MaxBlockRange;
         private float BlockRange;
 
-        private float MinStockAmount = 1;
-        private float MaxStockAmount = 50;
+        private float MinStockAmount;
+        private float MaxStockAmount;
         private float StockAmount;
 
         private bool ShowArea = false;
@@ -153,6 +158,10 @@ namespace InventoryTether
         {
             try
             {
+                Config.Load();
+
+                RetrieveValuesFromConfig();
+
                 InventoryTetherBlock = (IMyCollector)Entity;
 
                 if (InventoryTetherBlock.CubeGrid?.Physics == null)
@@ -173,8 +182,8 @@ namespace InventoryTether
 
                 if (SettingsBlockRange <= 0)
                 {
-                    BlockRange = 250;
-                    SettingsBlockRange = 250;
+                    BlockRange = MaxBlockRange / 2;
+                    SettingsBlockRange = MaxBlockRange / 2;
                 }
                 else
                     BlockRange = SettingsBlockRange;
@@ -185,7 +194,7 @@ namespace InventoryTether
                     SettingsStockAmount = 10;
                 }
                 else
-                    BlockRange = SettingsBlockRange;
+                    StockAmount = SettingsStockAmount;
 
                 if (SettingsHardCap != HardCap)
                 {
@@ -262,6 +271,28 @@ namespace InventoryTether
         #endregion
 
         #region Utilities
+        public void RetrieveValuesFromConfig()
+        {
+            // Assign General Values from Config
+            if (MinimumPowerRequirement != Config.MinimumPowerRequirement)
+                MinimumPowerRequirement = Config.MinimumPowerRequirement;
+
+            if (MaximumPowerRequirement != Config.MaximumPowerRequirement)
+                MaximumPowerRequirement = Config.MaximumPowerRequirement;
+
+            if (MinBlockRange != Config.MinBlockRange)
+                MinBlockRange = Config.MinBlockRange;
+
+            if (MaxBlockRange != Config.MaxBlockRange)
+                MaxBlockRange = Config.MaxBlockRange;
+
+            if (MinStockAmount != Config.MinStockAmount)
+                MinStockAmount = Config.MinStockAmount;
+
+            if (MaxStockAmount != Config.MaxStockAmount)
+                MaxStockAmount = Config.MaxStockAmount;
+        }
+
         private void SetStatus(string text, int aliveTime = 300, string font = MyFontEnum.Green)
         {
             if (NotifStatus == null)
@@ -291,13 +322,13 @@ namespace InventoryTether
             if (!InventoryTetherBlock.IsWorking)
                 return 0f;
 
-            else if (BlockRange <= 5f)
+            else if (BlockRange <= MinBlockRange)
             {
-                return 1.000f;
+                return MinimumPowerRequirement;
             }
             else
             {
-                float powerPrecentage = InventoryTetherBlockDef.RequiredPowerInput = 50f;
+                float powerPrecentage = InventoryTetherBlockDef.RequiredPowerInput = MaximumPowerRequirement;
                 float sliderValue = BlockRange;
 
                 float ratio = sliderValue / MaxBlockRange;
@@ -342,7 +373,7 @@ namespace InventoryTether
                 }
 
                 float radius = SettingsBlockRange / 2;
-                float lineThickness = 0.15f + ((radius - 2.5f) / (250 - 2.5f)) * (1.5f - 0.15f);
+                float lineThickness = 0.15f + ((radius - 2.5f) / ((MaxBlockRange / 2) - (MinBlockRange / 2))) * (1.5f - 0.15f);
 
                 MySimpleObjectDraw.DrawTransparentSphere(ref matrix, SettingsBlockRange / 2, ref color, MySimpleObjectRasterizer.Wireframe, wireDivRatio, null, MyStringId.GetOrCompute("WeaponLaserIgnoreDepth"), lineThickness, -1, null, BlendTypeEnum.SDR, 1);
             }
@@ -824,7 +855,7 @@ namespace InventoryTether
         {
             var logic = GetLogic(block);
             if (logic != null)
-                logic.BlockRange = MathHelper.Clamp(value, 5f, 500f);
+                logic.BlockRange = MathHelper.Clamp(value, logic.MinBlockRange, logic.MaxBlockRange);
             logic.BlockRange = (float)Math.Round(logic.BlockRange, 0);
             logic.SettingsBlockRange = logic.BlockRange;
         }
@@ -849,7 +880,7 @@ namespace InventoryTether
         {
             var logic = GetLogic(block);
             if (logic != null)
-                logic.StockAmount = MathHelper.Clamp(value, 1f, 50f);
+                logic.StockAmount = MathHelper.Clamp(value, logic.MinStockAmount, logic.MaxStockAmount);
             logic.StockAmount = (float)Math.Round(logic.StockAmount, 0);
             logic.SettingsStockAmount = logic.StockAmount;
         }
@@ -892,6 +923,206 @@ namespace InventoryTether
             logic.Settings.HardCap = logic.HardCap;
         }
         #endregion
+    }
+
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Collector), false, "Quantum_Tether")]
+    public class TetherRingsAnimation : MyGameLogicComponent
+    {
+        private const string SubpartOneName = "Torus_1"; // dummy name without the "subpart_" prefix
+        private const float SubOne_DegreesPerTick = 10f; // rotation per tick in degrees (60 ticks per second)
+        private const float SubOne_AccelPercentPerTick = 0.05f; // aceleration percent of "DEGREES_PER_TICK" per tick.
+        private const float SubOne_DeaccelPercentPerTick = 0.0035f; // deaccleration percent of "DEGREES_PER_TICK" per tick.
+        private readonly Vector3 SubOne_RotAxis = Vector3.Forward; // rotation axis for the subpart, you can do new Vector3(0.0f, 0.0f, 0.0f) for custom values
+        private const float SubOne_MaxDistSq = 1000 * 1000; // player camera must be under this distance (squared) to see the subpart spinning
+
+        private const string SubpartTwoName = "Torus_2"; // dummy name without the "subpart_" prefix
+        private const float SubTwo_DegreesPerTick = 7.5f; // rotation per tick in degrees (60 ticks per second)
+        private const float SubTwo_AccelPercentPerTick = 0.035f; // aceleration percent of "DEGREES_PER_TICK" per tick.
+        private const float SubTwo_DeaccelPercentPerTick = 0.005f; // deaccleration percent of "DEGREES_PER_TICK" per tick.
+        private readonly Vector3 SubTwo_RotAxis = Vector3.Left; // rotation axis for the subpart, you can do new Vector3(0.0f, 0.0f, 0.0f) for custom values
+        private const float SubTwo_MaxDistSq = 1000 * 1000; // player camera must be under this distance (squared) to see the subpart spinning
+
+        private const string SubpartThreeName = "Torus_3"; // dummy name without the "subpart_" prefix
+        private const float SubThree_DegreesPerTick = 5f; // rotation per tick in degrees (60 ticks per second)
+        private const float SubThree_AccelPercentPerTick = 0.025f; // aceleration percent of "DEGREES_PER_TICK" per tick.
+        private const float SubThree_DeaccelPercentPerTick = 0.0075f; // deaccleration percent of "DEGREES_PER_TICK" per tick.
+        private readonly Vector3 SubThree_RotAxis = Vector3.Up; // rotation axis for the subpart, you can do new Vector3(0.0f, 0.0f, 0.0f) for custom values
+        private const float SubThree_MaxDistSq = 1000 * 1000; // player camera must be under this distance (squared) to see the subpart spinning
+
+        private IMyFunctionalBlock block;
+
+        private bool SubOneFirstFind = true;
+        private Matrix SubOneLocalMatrix; // keeping the matrix here because subparts are being re-created on paint, resetting their orientations
+        private float SubOneTargetSpeedMultiplier; // used for smooth transition
+
+        private bool SubTwoFirstFind = true;
+        private Matrix SubTwoLocalMatrix; // keeping the matrix here because subparts are being re-created on paint, resetting their orientations
+        private float SubTwoTargetSpeedMultiplier; // used for smooth transition
+
+        private bool SubThreeFoundFirst = true;
+        private Matrix SubThreeLocalMatrix; // keeping the matrix here because subparts are being re-created on paint, resetting their orientations
+        private float SubThreeTargetSpeedMultiplier; // used for smooth transition
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            if (MyAPIGateway.Utilities.IsDedicated)
+                return;
+
+            block = (IMyFunctionalBlock)Entity;
+
+            if (block.CubeGrid?.Physics == null)
+                return;
+
+            NeedsUpdate = MyEntityUpdateEnum.EACH_FRAME;
+        }
+
+        public override void UpdateBeforeSimulation()
+        {
+            try
+            {
+                SpinSubpartOne();
+                SpinSubpartTwo();
+                SpinSubpartThree();
+
+            }
+            catch (Exception e)
+            {
+                AddToLog(e);
+            }
+        }
+
+        private void SpinSubpartOne()
+        {
+            bool shouldSpin = block.IsWorking; // if block is functional and enabled and powered.
+
+            if (!shouldSpin && Math.Abs(SubOneTargetSpeedMultiplier) < 0.00001f)
+                return;
+
+            if (shouldSpin && SubOneTargetSpeedMultiplier < 1)
+            {
+                SubOneTargetSpeedMultiplier = Math.Min(SubOneTargetSpeedMultiplier + SubOne_AccelPercentPerTick, 1);
+            }
+            else if (!shouldSpin && SubOneTargetSpeedMultiplier > 0)
+            {
+                SubOneTargetSpeedMultiplier = Math.Max(SubOneTargetSpeedMultiplier - SubOne_DeaccelPercentPerTick, 0);
+            }
+
+            var camPos = MyAPIGateway.Session.Camera.WorldMatrix.Translation; // local machine camera position
+
+            if (Vector3D.DistanceSquared(camPos, block.GetPosition()) > SubOne_MaxDistSq)
+                return;
+
+            MyEntitySubpart subpart;
+            if (Entity.TryGetSubpart(SubpartOneName, out subpart)) // subpart does not exist when block is in build stage
+            {
+                if (SubOneFirstFind) // first time the subpart was found
+                {
+                    SubOneFirstFind = false;
+                    SubOneLocalMatrix = subpart.PositionComp.LocalMatrixRef;
+                }
+
+                if (SubOneTargetSpeedMultiplier > 0)
+                {
+                    SubOneLocalMatrix *= Matrix.CreateFromAxisAngle(SubOne_RotAxis, MathHelper.ToRadians(SubOneTargetSpeedMultiplier * SubOne_DegreesPerTick));
+                    SubOneLocalMatrix = Matrix.Normalize(SubOneLocalMatrix); // normalize to avoid any rotation inaccuracies over time resulting in weird scaling
+                }
+
+                subpart.PositionComp.SetLocalMatrix(ref SubOneLocalMatrix);
+            }
+        }
+
+        private void SpinSubpartTwo()
+        {
+            bool shouldSpin = block.IsWorking; // if block is functional and enabled and powered.
+
+            if (!shouldSpin && Math.Abs(SubTwoTargetSpeedMultiplier) < 0.00001f)
+                return;
+
+            if (shouldSpin && SubTwoTargetSpeedMultiplier < 1)
+            {
+                SubTwoTargetSpeedMultiplier = Math.Min(SubTwoTargetSpeedMultiplier + SubTwo_AccelPercentPerTick, 1);
+            }
+            else if (!shouldSpin && SubTwoTargetSpeedMultiplier > 0)
+            {
+                SubTwoTargetSpeedMultiplier = Math.Max(SubTwoTargetSpeedMultiplier - SubTwo_DeaccelPercentPerTick, 0);
+            }
+
+            var camPos = MyAPIGateway.Session.Camera.WorldMatrix.Translation; // local machine camera position
+
+            if (Vector3D.DistanceSquared(camPos, block.GetPosition()) > SubTwo_MaxDistSq)
+                return;
+
+            MyEntitySubpart subpart;
+            if (Entity.TryGetSubpart(SubpartTwoName, out subpart)) // subpart does not exist when block is in build stage
+            {
+                if (SubTwoFirstFind) // first time the subpart was found
+                {
+                    SubTwoFirstFind = false;
+                    SubTwoLocalMatrix = subpart.PositionComp.LocalMatrixRef;
+                }
+
+                if (SubTwoTargetSpeedMultiplier > 0)
+                {
+                    SubTwoLocalMatrix *= Matrix.CreateFromAxisAngle(SubTwo_RotAxis, MathHelper.ToRadians(SubTwoTargetSpeedMultiplier * SubTwo_DegreesPerTick));
+                    SubTwoLocalMatrix = Matrix.Normalize(SubTwoLocalMatrix); // normalize to avoid any rotation inaccuracies over time resulting in weird scaling
+                }
+
+                subpart.PositionComp.SetLocalMatrix(ref SubTwoLocalMatrix);
+            }
+        }
+
+        private void SpinSubpartThree()
+        {
+            bool shouldSpin = block.IsWorking; // if block is functional and enabled and powered.
+
+            if (!shouldSpin && Math.Abs(SubThreeTargetSpeedMultiplier) < 0.00001f)
+                return;
+
+            if (shouldSpin && SubThreeTargetSpeedMultiplier < 1)
+            {
+                SubThreeTargetSpeedMultiplier = Math.Min(SubThreeTargetSpeedMultiplier + SubThree_AccelPercentPerTick, 1);
+            }
+            else if (!shouldSpin && SubThreeTargetSpeedMultiplier > 0)
+            {
+                SubThreeTargetSpeedMultiplier = Math.Max(SubThreeTargetSpeedMultiplier - SubThree_DeaccelPercentPerTick, 0);
+            }
+
+            var camPos = MyAPIGateway.Session.Camera.WorldMatrix.Translation; // local machine camera position
+
+            if (Vector3D.DistanceSquared(camPos, block.GetPosition()) > SubThree_MaxDistSq)
+                return;
+
+            MyEntitySubpart subpart;
+            if (Entity.TryGetSubpart(SubpartThreeName, out subpart)) // subpart does not exist when block is in build stage
+            {
+                if (SubThreeFoundFirst) // first time the subpart was found
+                {
+                    SubThreeFoundFirst = false;
+                    SubThreeLocalMatrix = subpart.PositionComp.LocalMatrixRef;
+                }
+
+                if (SubThreeTargetSpeedMultiplier > 0)
+                {
+                    SubThreeLocalMatrix *= Matrix.CreateFromAxisAngle(SubThree_RotAxis, MathHelper.ToRadians(SubThreeTargetSpeedMultiplier * SubThree_DegreesPerTick));
+                    SubThreeLocalMatrix = Matrix.Normalize(SubThreeLocalMatrix); // normalize to avoid any rotation inaccuracies over time resulting in weird scaling
+                }
+
+                subpart.PositionComp.SetLocalMatrix(ref SubThreeLocalMatrix);
+            }
+        }
+
+        private void AddToLog(Exception e)
+        {
+            MyLog.Default.WriteLineAndConsole($"ERROR {GetType().FullName}: {e.ToString()}");
+
+            if (MyAPIGateway.Session?.Player != null)
+                MyAPIGateway.Utilities.ShowNotification($"[ ERROR: {GetType().FullName}: {e.Message} | Send SpaceEngineers.Log to mod author ]", 10000, MyFontEnum.Red);
+        }
     }
 
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Collector), false, "Quantum_Tether")]
